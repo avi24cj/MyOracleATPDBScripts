3c4dc4a962f897fda4aef23b1d4226dbc1839d90
Avisek <das.avisek@gmail.com>
2026-01-18 13:47:17 -0600

    Customers

---
+++ customer.sql
@@ -0,0 +1,9 @@
+FINALTESTDB	1760808384596	SQL	1	0.007
+insert into customer
+(cust_id,cust_name,dob,address)
+values(103,'Test User3',TO_DATE('15/08/2006','dd/mm/yyyy'),'Test Addr 2399');
+HRDB	1722899907418	SQL	1	0.017
+insert into customer
+(CUST_ID,cust_name,dob,mobile)
+values
+(100002,'Radha',to_date('09/24/2012','mm/dd/yyyy'),1348374989);
