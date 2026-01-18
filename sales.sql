911eed6b38baad202681c6e6291b8eed2a7f7ba6
Avisek <das.avisek@gmail.com>
2026-01-18 13:53:45 -0600

    Sales

---
+++ sales.sql
@@ -0,0 +1,12 @@
+MYTESTDB	1765729374073	SQL	1	0.056
+insert into sales values(1,'Apple Originals1','Macbook pro 13',3,2500);
+insert into sales values(1,'Apple Originals1','Airpods pro',5,3500);
+insert into sales values(2,'Apple Originals2','Macbook pro 13',1,900);
+insert into sales values(2,'Apple Originals2','Airpods pro',2,1400);
+insert into sales values(3,'Apple Originals3','Macbook pro 13',5,4000);
+insert into sales values(3,'Apple Originals3','Airpods pro',1,700);
+insert into sales values(3,'Apple Originals3','iphone 13 pro',1,1300);
+insert into sales values(3,'Apple Originals3','iphone 14 pro',2,2500);
+insert into sales values(4,'Apple Originals4','Macbook pro 13',3,2400);
+insert into sales values(4,'Apple Originals4','Airpods pro',3,2100);
+insert into sales values(4,'Apple Originals4','iphone 13 pro',2,2600);
