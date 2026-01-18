8a4cd2388c68ee5f4a029dfd97468d64ae10dc79
Avisek <das.avisek@gmail.com>
2026-01-18 13:52:44 -0600

    Regions

---
+++ OT_REGIONS.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------
+--  OT
+--------------------------------------------------------
+REM INSERTING into OT.REGIONS
+SET DEFINE OFF;
+Insert into OT.REGIONS (REGION_ID,REGION_NAME) values (1,'Europe');
+Insert into OT.REGIONS (REGION_ID,REGION_NAME) values (2,'Americas');
+Insert into OT.REGIONS (REGION_ID,REGION_NAME) values (3,'Asia');
+Insert into OT.REGIONS (REGION_ID,REGION_NAME) values (4,'Middle East and Africa');
